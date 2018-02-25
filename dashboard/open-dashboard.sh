4a82ca42529b2b361997f05910b2908687a71892
Anuradha Weeraman <anuradha@weeraman.com>
2018-02-25 09:48:39 -0500

    Updated to use chrome

--- dashboard/open-dashboard.sh
+++ dashboard/open-dashboard.sh
@@ -1,2 +1,2 @@
 kubectl proxy &
-open http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/
+open -a "/Applications/Google Chrome.app" 'http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/'
